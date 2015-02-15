9209a6791a53ea9e9f05ba8817ad7d164bb7c61c
Ashutosh Chandane <ashutoshchandane7390@gmail.com>
2015-02-14 21:21:07 -0800

    first commit

---
+++ src/com/example/plumcalculator/CalculatorActivity.java
@@ -0,0 +1,159 @@
+package com.example.plumcalculator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CalculatorActivity extends ActionBarActivity {
+    TextView disp;
+    static int result=0;
+    private int NumberBf;
+    private String firstOperator = "";
+    private String initialOperator="";
+    boolean isOperation=false;
+    public ButtonClickListener btnClick = new ButtonClickListener();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+        disp=(TextView)findViewById(R.id.textView);
+        disp.setText("");
+
+        final int idList[]={R.id.button,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,
+                R.id.button8,R.id.button9,R.id.button10,R.id.button11,R.id.button12,R.id.button13,R.id.button14};
+        for(int id:idList){
+            View v=(View)findViewById(id);
+            v.setOnClickListener(btnClick);
+        }
+    }
+
+    public void mMath(String op){
+    		NumberBf = Integer.parseInt(disp.getText().toString());// save the screen
+    		mResult();
+            if(op.equals("=")) {
+                NumberBf = 0;
+                result = 0;
+                firstOperator="=";
+            }
+    		
+    }
+
+public void getKeyboard(String numb){
+    String ScrCurrent= disp.getText().toString();
+    
+    if(ScrCurrent.equals("0")) {
+        ScrCurrent="";
+    }
+    // to avoid adding numbers after equals
+	if(!firstOperator.equals("=") && initialOperator.equals("")) {
+	    ScrCurrent += numb;
+	    disp.setText(ScrCurrent);
+	}
+
+}
+    public void mResult(){
+        if(initialOperator.equals("-")){
+        	NumberBf = -NumberBf;
+        }
+    	if(firstOperator.equals("+") || firstOperator.equals("")){
+            result = result + NumberBf;
+        }
+        if(firstOperator.equals("-")){
+                result =result - NumberBf;
+        }
+        if(firstOperator.equals("="))
+        {
+            result=NumberBf;
+
+        }
+            disp.setText(String.valueOf(result));
+
+
+    }
+// new class ButtonClickListener
+    private class ButtonClickListener implements View.OnClickListener{
+    @Override
+    public void onClick(View v){
+        //check
+
+            switch (v.getId()) {
+                case R.id.button13: //clear screen
+                    result = 0;
+                    disp.setText("0");
+                    NumberBf = 0;
+                    firstOperator = "";
+                    break;
+                case R.id.button4: // plus
+                    if(disp.getText().toString().equals("")){
+                    	initialOperator = "+";
+                    }
+                    else{
+                    	if(!isOperation) {
+                            mMath("+");
+                        }
+                        firstOperator = "+";
+                        isOperation = true;
+                    }
+                    break;
+                case R.id.button10:
+                	if(disp.getText().toString().equals("")){
+                    	initialOperator = "-";
+                    }
+                    else{
+                    	if(!isOperation)
+                        {
+                            mMath("-");
+                        }
+                        firstOperator = "-";
+                        isOperation = true;
+                    }
+                    break;
+                case R.id.button14://equals
+                	if(result !=0){
+                		if (isOperation) {
+                            break;
+                        }
+                        mMath("=");
+                    }
+                         break;
+                default:
+                    String numb = ((Button) v).getText().toString();
+                    if (isOperation) { // isoperator means you have a operator and then a number.
+                        disp.setText(""); // set screen to blank to show next number
+                        isOperation = false;
+                    }
+
+                    getKeyboard(numb);
+                    break;
+            }
+
+    }
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.calculator, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
